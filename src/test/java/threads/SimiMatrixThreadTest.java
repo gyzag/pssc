34096a226dd14fa518e3eb6d3ff6735342847cf1
gyz6666 <773803906@qq.com>
2018-01-24 22:32:13 +0800

    Create test of building similar matrix

---
+++ src/test/java/threads/SimiMatrixThreadTest.java
@@ -0,0 +1,82 @@
+package threads;
+
+import data.DataConnector;
+import data.DataConnector4File;
+import data.Point;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author GYZ
+ * @DESCRIPTION
+ * @create 2018-01-23 18:40
+ **/
+public class SimiMatrixThreadTest {
+  // init dataConnector
+  private static DataConnector dataConnector = DataConnector4File.getInstance();
+
+  /*
+    * 
+    * Calculate time cost of parallel computing
+    * @author gyz
+    * @date 2018/1/24 22:15
+    * @param [the number of threads]
+    * @return long  
+    */  
+  public static long parallelCompSimiMatrix (int thNum) throws  IOException, InterruptedException{
+    System.out.println("Starting parallel computing");
+    long t1 = System.currentTimeMillis();
+    List<List<Point>> trajs = dataConnector.getTrajData();
+    int len = trajs.size();
+    double[][] w = new double[len][len];
+    // Threads pool
+    ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+    CountDownLatch threadSignal = new CountDownLatch(thNum);
+    // Starting calculating
+    for(int i = 0; i < thNum; i++){
+      cachedThreadPool.execute(new SimiMatrixThread(20, w, trajs, (i * len) / thNum,
+          (i + 1) * len / thNum, threadSignal));
+    }
+    threadSignal.await();
+    long t2 = System.currentTimeMillis();
+    // Ending multi threading calculating
+    System.out.println("Ending multi threading calculating");
+    return t2 - t1;
+  }
+
+  /*
+    *
+    * Calculate time cost of serial computing
+    * @author gyz
+    * @date 2018/1/24 22:19
+    * @param []
+    * @return long
+    */
+  public static long serialCompSimiMatrix() throws  IOException, InterruptedException{
+    System.out.println("Starting serial computing");
+    long t1 = System.currentTimeMillis();
+    List<List<Point>> trajs = dataConnector.getTrajData();
+    int len = trajs.size();
+    double[][] w = new double[len][len];
+    CountDownLatch singleSignal = new CountDownLatch(1);
+    Thread th = new SimiMatrixThread(20, w, trajs, 0,
+        len, singleSignal);
+    th.start();
+    singleSignal.await();
+    long t2 = System.currentTimeMillis();
+    System.out.println("Ending serial computing");
+    return t2 - t1;
+  }
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    System.out.println("-----START TEST-----");
+    long pTime = parallelCompSimiMatrix(8);
+    long sTime = serialCompSimiMatrix();
+    System.out.println("Speedup = " + 1.0 *  sTime / pTime );
+    System.out.println("-----END TEST-------");
+  }
+
+}
