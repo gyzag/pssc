e2cae389049c3c57f36870d50a5b27691f46bb56
gyz6666 <773803906@qq.com>
2018-02-01 23:42:11 +0800

    Add declaration of variables

--- src/main/java/math/KMeans.java
+++ src/main/java/math/KMeans.java
@@ -2,9 +2,33 @@ package math;
 
 /**
  * @author GYZ
- * @DESCRIPTION
+ * @DESCRIPTION KMeans-cluster
  * @create 2018-01-27 21:32
  **/
 public class KMeans {
 
+  /**
+   *  Numbers of clusters
+   */
+  private int k;
+
+  /**
+   *  Maximum number of iterations
+   */
+  private int max;
+
+  /**
+   * The Input matrix
+   */
+  private double[][] m;
+
+  /**
+   * The centers of clusters
+   */
+  private double[] center;
+
+  /**
+   *  The label of data
+   */
+  private int[] label;
 }
