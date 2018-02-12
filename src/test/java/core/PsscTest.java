1512321544990c1bfff466863a9be35b43010b71
gyz6666 <773803906@qq.com>
2018-02-12 21:56:01 +0800

    Complete PsscTest

--- src/test/java/core/PsscTest.java
+++ src/test/java/core/PsscTest.java
@@ -1,10 +1,40 @@
 package core;
 
+import data.DataConnector;
+import data.DataConnector4File;
+import data.Point;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author GYZ
  * @DESCRIPTION
  * @create 2018-01-29 20:44
  **/
 public class PsscTest {
+
+  // init dataConnector
+  private static DataConnector dataConnector = DataConnector4File.getInstance();
+
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    System.out.println("----------START TEST----------");
+    // init data
+    List<List<Point>> trajs = dataConnector.getTrajData();
+    int k = 4;
+    Pssc pssc = new Pssc(k, trajs);
+
+    int[] labels = pssc.getLabels();
+    List<Integer> nums = new ArrayList<>();
+    for(int i : labels)
+      if(!nums.contains(i))
+        nums.add(i);
+    System.out.println("Correct answer is "  + k );
+    System.out.println( "My answer is "+ nums.size());
+    System.out.println("----------END TEST----------");
+  }
+
+
 
 }
